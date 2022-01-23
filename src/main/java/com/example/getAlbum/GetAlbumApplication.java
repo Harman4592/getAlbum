2aacc8e615bd87c8594212f92f85a62615470299
Harmanpreet Kaur <harmanpreetkaur0507@gmail.com>
2022-01-23 15:24:30 -0500

    First Commit

---
+++ src/main/java/com/example/getAlbum/GetAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.example.getAlbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetAlbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/getAlbum/GetAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getAlbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
